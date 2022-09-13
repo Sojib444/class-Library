b926df9946d7a9866b7a84adc66a96a553481b89
Sojib <mdsojibhosen444@gmail.com>
2022-09-13 11:33:20 +0600

    commit

---
+++ DemoLibrary/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace DemoLibrary
+{
+    public class Class1
+    {
+        public static string Name()
+        {
+            return "sojib";
+        }
+    }
+}
---
+++ UI/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using DemoLibrary;
+
+string name = Class1.Name();
+Console.WriteLine( name);
